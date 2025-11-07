e0471b7dd325b3f255b3d5a89450573ec7aa286e
Saloni <s69056298@gmail.com>
2025-11-07 23:38:40 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import Habits from './Habits';
-
 const Dashboard = () => {
     const handleLogout = () => {
         localStorage.removeItem('token');
         window.location.href = '/login';
     };
-
-    return (
-        <div>
+ return (
+ <div>
             <h2>Welcome to Your Dashboard!</h2>
             <Habits />
             <button onClick={handleLogout}>Logout</button>
         </div>
     );
 };
-
 export default Dashboard;
